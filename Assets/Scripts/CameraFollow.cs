dfef9afefff013e75cf95db89ec20934d7dbd6be
Lengad <daniel.ehrler@gmail.com>
2020-02-01 13:43:59 +0100

    add Player

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -32,13 +32,11 @@ public class CameraFollow : MonoBehaviour
     }
 
     private void Update()
-    {
-        float inputX = Input.GetAxis("RightStickHorizontal");
-        float inputZ = Input.GetAxis("RightStickVertical");
+    {      
         mouseX = Input.GetAxis("Mouse X");
         mouseY = Input.GetAxis("Mouse Y");
-        finalInputX = inputX + mouseX;
-        finalInputZ = inputZ + mouseY;
+        finalInputX =  mouseX;
+        finalInputZ =  mouseY;
 
         rotY += finalInputX * inputSensitivity * Time.deltaTime;
         rotX += finalInputZ * inputSensitivity * Time.deltaTime;
