119462019f48fe229448519492558423d77e06da
Daniel Ehrler <daniel.ehrler@gmail.com>
2020-02-02 14:58:38 +0100

    add Ball bounce sound

---
+++ Assets/Scripts/BounceSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BounceSound : MonoBehaviour
+{
+    AudioSource audioData;
+
+    private void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        audioData.Play(0);
+    }
+}
