0c5ac5b5d9fe6dd88953ecaba33a4d575539b252
Philipp <philipp.groebelbauer@gmail.com>
2020-02-02 14:52:51 +0100

    initial ball positions & ball respawn if lost in lava

--- Assets/Scripts/BallSpawn.cs
+++ Assets/Scripts/BallSpawn.cs
@@ -6,10 +6,12 @@ public class BallSpawn : MonoBehaviour
 {
     private GameObject[] ballSpawnPoints;
     public float maxSpawnOffset;
+    Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         ballSpawnPoints = new GameObject[2];
         ballSpawnPoints[0] = GameObject.Find("BallSpawnPoint_Blue");
         ballSpawnPoints[1] = GameObject.Find("BallSpawnPoint_Red");
@@ -21,13 +23,15 @@ public class BallSpawn : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
+        Debug.Log("Ball in Lava");
         if (collision.transform.CompareTag("Lava"))
         {
             // Pick one of the two sides randomly
             int r = Random.Range(0, 1);
             transform.position = new Vector3(Random.Range(-maxSpawnOffset, maxSpawnOffset) + ballSpawnPoints[r].transform.position.x, ballSpawnPoints[r].transform.position.y, Random.Range(-maxSpawnOffset, maxSpawnOffset) + ballSpawnPoints[r].transform.position.z);
+            rb.velocity = Vector3.zero;
         }
     }
 }
